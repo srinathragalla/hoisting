a6cbdd7e5ce466ba44115cc29843ebcf548dfa95
srinath ragalla <srinathragalla17@gmail.com>
2021-12-17 09:28:35 +0530

    hoisting

---
+++ hoistng.js
@@ -0,0 +1,10 @@
+function myName(name) { 
+    console.log("My name is " + name);
+}
+myName("srinath")
+
+myCar("porsche");
+function myCar(name) {
+ console.log("my car name is:" + " " + name);
+  
+}
